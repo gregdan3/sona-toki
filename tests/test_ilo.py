5ea2bad6eb2c381909a93eb9433f17b127e897df
Gregory Danielson <gregory.danielson3@gmail.com>
2024-04-24 17:02:46 -0500

    o wawa e alasa sona pi pona ilo

--- tests/test_ilo.py
+++ tests/test_ilo.py
@@ -8,20 +8,43 @@ from otokipona.Filters import (
     ProperName,
     Punctuations,
 )
-from otokipona.Scorers import PassFail
+from otokipona.Scorers import Scaling, SoftScaling
 from otokipona.Cleaners import ConsecutiveDuplicates
 from otokipona.Tokenizers import word_tokenize_tok
-from otokipona.Preprocessors import URLs, DiscordEmotes
+from otokipona.Preprocessors import (
+    URLs,
+    DiscordEmotes,
+    DiscordSpecial,
+    DiscordChannels,
+    DiscordMentions,
+)
 
 
 def test_constructor():
     ilo = Ilo(
-        preprocessors=[URLs, DiscordEmotes],
+        preprocessors=[
+            URLs,
+            DiscordEmotes,
+            DiscordMentions,
+            DiscordChannels,
+            DiscordSpecial,
+        ],
         ignoring_filters=[Numerics, Punctuations],
-        scoring_filters=[NimiLinku, Syllabic, Alphabetic, ProperName],
+        scoring_filters=[NimiLinku, Syllabic, ProperName, Alphabetic],
         cleaners=[ConsecutiveDuplicates],
-        scorer=PassFail,
+        scorer=SoftScaling,
         tokenizer=word_tokenize_tok,
+        passing_score=0.8,
     )
     assert not ilo.is_toki_pona("super bruh moment 64")
     assert ilo.is_toki_pona("mi unpa e mama sina")
+    assert ilo.is_toki_pona("mama sina li mu tan mi")
+    assert ilo.is_toki_pona("toki. sike li pona ala. o anpa.")
+    assert ilo.is_toki_pona("musi Homestuck li ike tawa mi")
+    assert ilo.is_toki_pona("mi mtue o kama sona")
+    assert ilo.is_toki_pona("ni li tenpo penpo")
+    assert ilo.is_toki_pona("ni li tptpt")
+
+    assert not ilo.is_toki_pona("I'm Trying To Evade The Filter")
+    assert not ilo.is_toki_pona("""aaa i non-saw usa's most multiple element-set
+it's as asinine as in `e`-less speak""")
