c13afadf26999e04861d0f05de116a6bbfe24c51
Gregory Danielson <gregory.danielson3@gmail.com>
2024-05-03 16:52:24 -0500

    o pona e alasa pi sona ilo

--- tests/test_ilo.py
+++ tests/test_ilo.py
@@ -36,18 +36,38 @@ def test_constructor():
         tokenizer=word_tokenize_tok,
         passing_score=0.8,
     )
-    ilo.debug = True
-    assert not ilo.is_toki_pona("super bruh moment 64")
+    # ilo._logging_threshold = 0.8
     assert ilo.is_toki_pona("mi unpa e mama sina")
-    assert ilo.is_toki_pona("mama sina li mu tan mi")
-    assert ilo.is_toki_pona("toki. sike li pona ala. o anpa.")
+    # toki pona
+    assert ilo.is_toki_pona("mama sina li lon seme? mi wile toki tawa ona")
+    assert ilo.is_toki_pona("sina sike pakala")
+    # names
     assert ilo.is_toki_pona("musi Homestuck li ike tawa mi")
+    # typoes
     assert ilo.is_toki_pona("mi mtue o kama sona")
+    assert ilo.is_toki_pona("mi mute o kma son")
+    # phonotactically valid
     assert ilo.is_toki_pona("ni li tenpo penpo")
+    # alphabetically valid
     assert ilo.is_toki_pona("ni li tptpt")
+    # a single
+    assert ilo.is_toki_pona("sipisi")
 
-    assert not ilo.is_toki_pona("I'm Trying To Evade The Filter")
+    # soft scaling with syllablic filter at 2/4 will pass up to 5 syllablic words
+    assert ilo.is_toki_pona("walawa malama walama malama mupi")
+    # but fail 6 or more
+    assert not ilo.is_toki_pona("manama manama namana namana majani makala")
+
+    # TODO: should soft scaling save an alphabetically valid single word?
+    assert not ilo.is_toki_pona("tok")
+    assert not ilo.is_toki_pona("mtue")
+
+    # just english
+    assert not ilo.is_toki_pona("bong")
+    assert not ilo.is_toki_pona("super bruh moment 64")
+    # all names
+    assert not ilo.is_toki_pona("I Want To Evade The Filter")
+    # all alphabetic
     assert not ilo.is_toki_pona(
-        """aaa i non-saw usa's most multiple element-set
-it's as asinine as in `e`-less speak"""
+        "aaa i non-saw usa's most multiple element-set. it's as asinine as in `e`-less speak"
     )
