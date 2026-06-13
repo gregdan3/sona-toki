77b4edb446a0f5d6af780e5b95c25d5e11094834
Gregory Danielson <gregory.danielson3@gmail.com>
2026-06-12 23:29:51 -0500

    pali: o ken e nasin sitelen lili

--- src/sonatoki/constants/writing.py
+++ src/sonatoki/constants/writing.py
@@ -6,11 +6,15 @@ ALPHABET = VOWELS + CONSONANTS
 """Commonly occurring strings which are some kind of valid Toki Pona or
 external token."""
 ALLOWABLES = {
-    # "x",  # ala
-    # "y",  # anu
+    "anusem",  # clipping of anu seme
     "kxk",  # ken ala ken
-    "wxw",  # wile ala wile
-    "msa",
-    "anusem",
+    "msa",  # mi sona ala
     "pon",  # pona
+    # these may be short enough to be an issue
+    # but if they aren't included here, they'll get a zero...
+    "tp",  # toki pona
+    "sp",  # sina pona, sitelen pona
+    "tpt",  # toki pona taso
+    "wxw",  # wile ala wile
+    "mptp",  # ma pona pi toki pona
 }
